280807cca66fffb731aebbd48fae596de59feac6
Nestor Martínez <nmartinezc1800@alumno.ipn.mx>
2024-03-02 10:08:31 -0600

    test

---
+++ 2. Text Normalization/testing.py
@@ -0,0 +1,18 @@
+from scraper import LaJornada, Expansion
+from consolidation import Consolidation
+from normalization import Normalization
+
+sections = ['Deportes', 'Economía', 'Ciencias', 'Cultura']
+new_object = LaJornada()
+new_object.getSectionsUrl(sections=sections)
+new_object.downloadNews()
+
+expansion = Expansion()
+expansion.getSectionsUrl()
+expansion.downloadNews()
+
+consolidation = Consolidation()
+consolidation.consolidate()
+
+normalization = Normalization()
+normalization.normalize()
